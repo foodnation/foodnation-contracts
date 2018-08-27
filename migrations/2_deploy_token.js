f99265a1484f6548b39c32977036d6eb86ccd2b6
Nikola Wyatt <nikola.wyatt@foodnation.io>
2018-08-27 18:56:40 -0300

    Fixing migrations

--- migrations/2_deploy_token.js
+++ migrations/2_deploy_token.js
@@ -1,9 +1,9 @@
 var contract = require('truffle-contract');
 
-var FoodNationTokenJson = require("build/contracts/FoodNationToken.json");
-var GnosisWalletJson = require("build/contracts/GnosisWallet.json");
-var PreSaleJson = require("build/contracts/PreSale.json");
-var FVestingTokenJson = require("build/contracts/VestingToken.json");
+var FoodNationTokenJson = require("../build/contracts/FoodNationToken.json");
+var GnosisWalletJson = require("../build/contracts/GnosisWallet.json");
+var PreSaleJson = require("../build/contracts/PreSale.json");
+var VestingTokenJson = require("../build/contracts/VestingToken.json");
 
 var nikola_wallet_address = '0x9c78aaec0be3d9ffd55bb0b6b7c61f69c2128a95';
 var neo_wallet_address = '0x91c2d785a28181b15decf26d9be812b0f7259a32';
@@ -40,66 +40,66 @@ function Reserved(address, amount) {
 module.exports = async function (deployer, network, accounts) {
     // Deploy FoodNation Token
     var FoodNationTokenContract = contract(FoodNationTokenJson);
-    await deployer.deploy(FoodNationToken, "FoodNation Token", "FOOD", 18, tokencap);
+    await deployer.deploy(FoodNationTokenContract, "FoodNation Token", "FOOD", 18, tokencap);
     const deployedToken = await FoodNationToken.deployed();
     console.log(deployedToken.address);
 
-    // Deploy Vesting Factory
-    await deployer.deploy(VestingFactory);
-    const deployedVestingFactory = await VestingFactory.deployed();
+    // // Deploy Vesting Factory
+    // await deployer.deploy(VestingFactory);
+    // const deployedVestingFactory = await VestingFactory.deployed();
 
-    // Vesting time for Founders
-    const timeNow = Math.floor(Date.now() / 1000);
-    const founder_start_vesting = timeNow + duration.minutes(10);
+    // // Vesting time for Founders
+    // const timeNow = Math.floor(Date.now() / 1000);
+    // const founder_start_vesting = timeNow + duration.minutes(10);
 
-    // Nikola Vesting Tokens
-    var NikolaVesting = deployedVestingFactory.newVesting("Nikola Wyatt", nikola_wallet_address, founder_start_vesting, duration.years(1), duration.years(1));
-    await deployer.deploy(NikolaVesting);
-    const deployedNikolaVesting = await NikolaVesting.deployed();
+    // // Nikola Vesting Tokens
+    // var NikolaVesting = deployedVestingFactory.newVesting("Nikola Wyatt", nikola_wallet_address, founder_start_vesting, duration.years(1), duration.years(1));
+    // await deployer.deploy(NikolaVesting);
+    // const deployedNikolaVesting = await NikolaVesting.deployed();
 
-    // Neo Vesting Tokens
-    var NeoVesting = deployedVestingFactory.newVesting("Neo Dula", neo_wallet_address, founder_start_vesting, duration.years(1), duration.years(1));
-    await deployer.deploy(NeoVesting);
-    const deployedNeoVesting = await NeoVesting.deployed();
+    // // Neo Vesting Tokens
+    // var NeoVesting = deployedVestingFactory.newVesting("Neo Dula", neo_wallet_address, founder_start_vesting, duration.years(1), duration.years(1));
+    // await deployer.deploy(NeoVesting);
+    // const deployedNeoVesting = await NeoVesting.deployed();
 
-    var reservedTokens = [];
-    reservedTokens.push(new Reserved(reserve_wallet_address, tokencap * 0.05));
-    reservedTokens.push(new Reserved(team_wallet_address, tokencap * 0.03));
-    reservedTokens.push(new Reserved(advisors_wallet_address, tokencap * 0.03));
-    reservedTokens.push(new Reserved(community_wallet_address, tokencap * 0.20));
-    reservedTokens.push(new Reserved(marketing_wallet_address, tokencap * 0.25));
-    reservedTokens.push(new Reserved(marketing_wallet_address, tokencap * 0.25));
-    reservedTokens.push(new Reserved(marketing_wallet_address, tokencap * 0.25));
-    reservedTokens.push(new Reserved(deployedNikolaVesting.address, tokencap * 0.045));
-    reservedTokens.push(new Reserved(deployedNeoVesting.address, tokencap * 0.045));
-    reservedTokens.push(new Reserved(pre_sale_wallet_address.address, tokencap * 0.05));
-    reservedTokens.push(new Reserved(sale_wallet_address.address, tokencap * 0.30));
+    // var reservedTokens = [];
+    // reservedTokens.push(new Reserved(reserve_wallet_address, tokencap * 0.05));
+    // reservedTokens.push(new Reserved(team_wallet_address, tokencap * 0.03));
+    // reservedTokens.push(new Reserved(advisors_wallet_address, tokencap * 0.03));
+    // reservedTokens.push(new Reserved(community_wallet_address, tokencap * 0.20));
+    // reservedTokens.push(new Reserved(marketing_wallet_address, tokencap * 0.25));
+    // reservedTokens.push(new Reserved(marketing_wallet_address, tokencap * 0.25));
+    // reservedTokens.push(new Reserved(marketing_wallet_address, tokencap * 0.25));
+    // reservedTokens.push(new Reserved(deployedNikolaVesting.address, tokencap * 0.045));
+    // reservedTokens.push(new Reserved(deployedNeoVesting.address, tokencap * 0.045));
+    // reservedTokens.push(new Reserved(pre_sale_wallet_address.address, tokencap * 0.05));
+    // reservedTokens.push(new Reserved(sale_wallet_address.address, tokencap * 0.30));
 
-    var addrs = [];
-    var amounts = [];
-    var reservedTokensLength = reservedTokens.length;
-    for (var i = 0; i < reservedTokensLength; i++) {
-        addrs.push(reservedTokens[i].address);
-        amounts.push(reservedTokens[i].amount);
-    }
+    // var addrs = [];
+    // var amounts = [];
+    // var reservedTokensLength = reservedTokens.length;
+    // for (var i = 0; i < reservedTokensLength; i++) {
+    //     addrs.push(reservedTokens[i].address);
+    //     amounts.push(reservedTokens[i].amount);
+    // }
 
-    deployedToken.setReservedTokensListMultiple(addrs, amounts);
-    deployedToken.distributeReservedTokens();
+    // deployedToken.setReservedTokensListMultiple(addrs, amounts);
+    // deployedToken.distributeReservedTokens();
 
 
 
-    const presale1_rate = 100;// Change to USD;
-    const presale_openingTime = Math.floor(new Date(2018, 8, 21, 0, 0, 0, 0) / 1000);
-    const presale_closingTime = Math.floor(new Date(2018, 11, 20, 23, 59, 59, 59) / 1000);
-    const presale1_cap = web3.toWei(10000000);// Change to tokens
+    // const presale1_rate = 100;// Change to USD;
+    // const presale_openingTime = Math.floor(new Date(2018, 8, 21, 0, 0, 0, 0) / 1000);
+    // const presale_closingTime = Math.floor(new Date(2018, 11, 20, 23, 59, 59, 59) / 1000);
+    // const presale1_cap = web3.toWei(10000000);// Change to tokens
 
-    await deployer.deploy(
-        PreSale1,
-        presale1_rate,
-        wallet,
-        deployedToken.address,
-        presale_openingTime,
-        presale_closingTime,
-        presale1_cap
-    );
+    // await deployer.deploy(
+    //     PreSale1,
+    //     presale1_rate,
+    //     wallet,
+    //     deployedToken.address,
+    //     presale_openingTime,
+    //     presale_closingTime,
+    //     presale1_cap
+    // );
 };
